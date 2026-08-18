9bec33582e9ecc65f68bed24df9b0483020f0092
fatsfa <fathifathima808@gmail.com>
2026-08-18 15:58:00 +0400

    frontend 10

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://flight-booking-m2av.onrender.com/api";
 
 function getStoredAccessToken() {
   return localStorage.getItem("token");
